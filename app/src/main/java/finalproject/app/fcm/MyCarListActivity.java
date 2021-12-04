4fc000448b49d1c8196f9849e651844c66c2000a
SARANG KIM <datda604@gmail.com>
2021-12-04 18:53:53 +0900

    2

--- app/src/main/java/finalproject/app/fcm/MyCarListActivity.java
+++ app/src/main/java/finalproject/app/fcm/MyCarListActivity.java
@@ -2,7 +2,9 @@ package finalproject.app.fcm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyCarListActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MyCarListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_car_list);
+    }
+    public void goMain(View v){
+        Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+        finish();
     }
 }
